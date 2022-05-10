c765494a0d8caba4d45c25826f293623a4ba9fcc
Łukasz Szawara <szawara.lukasz@gmail.com>
2022-05-10 19:04:53 +0200

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-
 from flask import Flask
 import subprocess
 from datetime import datetime
@@ -6,8 +5,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Hello :)'
-@app.route('/temperature/')
+@app.route('/temp/')
 def temp():
-        return "{\"temp\":"+subprocess.getoutput("cat /sys/devices/virtual/thermal/thermal_zone0/temp")+",\"datetime\":\""+str(datetime.now())+"\",\"sysInfo\":\"{"+subprocess.getoutput("screenfetch -n -N")+" "+subprocess.getoutput("df -h | grep ^/dev")+"}\"}"
+        return "{\"temp\":"+subprocess.getoutput("cat /sys/devices/virtual/thermal/thermal_zone0/temp")+",\"datetime\":\""+str(datetime.now())+"\",\"sysInfo\":\"{"+subprocess.getoutput("screenfetch -n -N")+"}\",\"hdd\":\"{" +subprocess.getoutput("df -h | grep ^/dev")+"\}\",\"who\":\"{"+subprocess.getoutput("who")+"}\"}"
 if __name__ == '__main__':
     app.run(debug=True, port=5000, host='0.0.0.0')
